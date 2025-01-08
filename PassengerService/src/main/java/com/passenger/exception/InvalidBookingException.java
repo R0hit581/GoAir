04fc54fbca50e172fa82c83099f80c62915bda48
R0hit <rohitkushwaha581@gmail.com>
2025-01-08 18:35:41 +0530

    added changes

--- PassengerService/src/main/java/com/passenger/exception/InvalidBookingException.java
+++
@@ -1,9 +0,0 @@
-package com.passenger.exception;
-
-
-public class InvalidBookingException extends RuntimeException {
-
-    public InvalidBookingException(String message) {
-        super(message);
-    }
-}
